cc6f8df269f11638fa1e3c54bd77c3a190e07fb3
Marian-Aziz <153827166+Marian-Aziz@users.noreply.github.com>
2024-01-11 12:44:59 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,45 @@
+
+// RECT.
+function rectangArea(x,y){
+    var total =(x+y)*2 ;
+    console.log(total);
+
+}
+    
+    
+        rectangArea(20,10);
+    
+
+    if(total <10) {
+return false;
+
+    }
+
+    //age days
+    function age(x){
+        var ageYears =x*360 ;
+        console.log(ageYears);
+    
+    }
+        
+        
+    age(20);
+        
+    
+        if(age<30) {
+    return false;
+    
+        }
+
+        //sum
+        function sum(x,y){
+            return (x+y) ;
+           
+        
+        }
+            
+        console.log(sum(10,20));
+      
+            
+        
+    //
